d05cdef747a7a0326ac5b31f93a7b19a7a45bb52
Iurii Volobuev <iurii.volobuev@gmail.com>
2026-04-21 00:02:30 +0200

    Extract urls to vars in dogs.html

--- sample-webapp/scripts/proxy.js
+++ sample-webapp/scripts/proxy.js
@@ -3,17 +3,17 @@
 //3. node proxy.js
 
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
 app.use('/', createProxyMiddleware({
-  target: 'http://localhost:8080/',
-  changeOrigin: true
+    target: 'http://localhost:8080/',
+    changeOrigin: true
 }));
 
 app.listen(3000, () => {
-  console.log('Proxy server is running on port 3000');
+    console.log('Proxy server is running on port 3000');
 });
